79562eddab43d50654f6e41467b7e348577ecf3a
juhis <jkarjala@broadinstitute.org>
2023-06-23 19:18:31 +0300

    favicon

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍝</text></svg>"></link>
       </Head>
 
       <main>
